74743d73344b9a175ca04101b02df56bc39c1c60
Ivan Senji <ivan.senji@gmail.com>
2010-03-18 22:30:42 +0100

    Initial commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,24 @@
+package controllers
+
+import play._
+import play.mvc._
+import play.data.validation.{Required, Validation}
+
+object Application extends Controller {
+
+  def index = {
+    println("index: calling render...")
+    render()
+  }
+
+  def sayHello(@Required myName : String) {
+    println("Application.sayHello")
+    if(Validation.hasErrors) {
+	println("validation errors")
+        flash.error("Oops, please enter your name!")
+        index
+    }
+    render(myName)
+  }
+
+}
