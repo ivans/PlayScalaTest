7c489fa4bb4198a07672bb5c1b0d9145f0a7b793
Ivan Senji <ivan.senji@gmail.com>
2010-03-21 22:04:55 +0100

    Dodao Bootstrap klasu koja dodaje testne podatke...

---
+++ app/Bootstrap.scala
@@ -0,0 +1,16 @@
+import play._
+import play.jobs._
+import play.test._
+ 
+import models._
+
+@OnApplicationStart
+class Bootstrap extends Job {
+ 
+    override def doJob() {
+        if(User.count() == 0) {
+            Fixtures.load("initial-data.yml");
+        }
+    }
+ 
+}
