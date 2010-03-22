59a06475928192c183b2ed7d8e28c450b5cd43b3
Ivan Senji <ivan.senji@gmail.com>
2010-03-22 22:48:06 +0100

    Sredjen layout i dodan css

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,36 +8,47 @@ import models._
 
 object Application extends Controller {
 
-  def index = {
-    println("index: calling render...")
-    val frontPost = Post.find("order by postedAt desc").first
-    val olderPosts = Post.find("order by postedAt desc").from(1).fetch(10);
-    render(frontPost, olderPosts);
-  }
+	@Before
+	def addDefaults() {
+		renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"))
+		renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"))
+	}
+	
+	def index = {
+		println("index: calling render...")
+		val frontPost = Post.find("order by postedAt desc").first
+		val olderPosts = Post.find("order by postedAt desc").from(1).fetch(10);
+		render(frontPost, olderPosts);
+	}
 
-  def sayHello(@Required myName : String) {
-    println("Application.sayHello")
-    if(Validation.hasErrors) {
-	println("validation errors")
-        flash.error("Oops, please enter your name!")
-        index
-    }
-    val userCount = User.count.asInstanceOf[Object]
-    render(myName, userCount)
-  }
+	def show(id : Long) {
+		val post = Post findById id
+		render(post)
+	}
+  
+	def sayHello(@Required myName : String) {
+		println("Application.sayHello")
+		if(Validation.hasErrors) {
+			println("validation errors")
+			flash.error("Oops, please enter your name!")
+			index
+		}
+		val userCount = User.count.asInstanceOf[Object]
+		render(myName, userCount)
+	}
 
-  def addUser(firstName : String, lastName : String, @Required email : String) {
-    println("Application.addUser")
-    if(Validation.hasErrors) {
-		println("validation errors")
-        flash.error("Enter username and email please!")
-        index
-    }
-    val user = new User(email, null, firstName, lastName, false)
-	user.saveThis()
-	flash.success("Saved new user: " + user)
-	println("Saved user")
-    index
-  }
+	def addUser(firstName : String, lastName : String, @Required email : String) {
+		println("Application.addUser")
+		if(Validation.hasErrors) {
+			println("validation errors")
+			flash.error("Enter username and email please!")
+			index
+		}
+		val user = new User(email, null, firstName, lastName, false)
+		user.saveThis()
+		flash.success("Saved new user: " + user)
+		println("Saved user")
+		index
+	}
 
 }
